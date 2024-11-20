91f1782ea6a7478affa1a0824d9167c239e2aa44
Deepthi Sandeep <deepththampi@hotmail.com>
2024-11-20 09:42:33 -0600

    Updated App.ts

--- server/src/schemas/index.ts
+++ server/src/schemas/index.ts
@@ -1,4 +1,4 @@
-import typeDefs from './typedefs';
-import resolvers from './resolvers';
+import typeDefs from './typedefs.js';
+import resolvers from './resolvers.js';
 
 export { typeDefs, resolvers };
